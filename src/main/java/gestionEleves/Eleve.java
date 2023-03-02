6221239363dcfe893e81c7bd5f7f2dc5997c8a3e
Khalid Taleb <khalidtaleb51001@gmail.com>
2023-03-02 18:15:24 +0100

    Partie1

--- src/main/java/gestionEleves/Eleve.java
+++ src/main/java/gestionEleves/Eleve.java
@@ -1,4 +1,51 @@
 package gestionEleves;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Eleve {
+    private String nom;
+    private ArrayList<Double> listeNotes;
+    private Double moyenne;
+
+    public Eleve(String nom) {
+        this.nom = nom;
+    }
+
+    public Double getMoyenne() {
+        return moyenne;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public ArrayList<Double> getListeNotes() {
+        return listeNotes;
+    }
+    public void ajouterNote(double note) throws NoteInvalide {
+//        try{
+//            if (note<0 || note>20)
+//                listeNotes.add(note);
+//        }
+//        catch (Exception NoteInavlide){
+//        }
+        if (note<0||note>20)
+            throw new NoteInvalide("La note inseree est invalide");
+        else listeNotes.add(note);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Eleve){
+            Eleve eleve=(Eleve) o;
+            return this.nom.equals(eleve.nom);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return nom + " (" + moyenne + ")";
+    }
 }
---
+++ src/main/java/gestionEleves/NoteInvalide.java
@@ -0,0 +1,7 @@
+package gestionEleves;
+
+public class NoteInvalide extends Exception {
+    public NoteInvalide(String msg) {
+        super(msg);
+    }
+}
